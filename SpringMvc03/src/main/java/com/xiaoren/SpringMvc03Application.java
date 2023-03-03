2feca6d4270256a21991e8174d0f974e4ec0eb80
xiaoren"git config --global user.email 2998024535@qq.comgit config --global user.name xiaoren <2998024535@qq.com>
2023-03-03 15:38:31 +0800

    SpringBoot项目配置

---
+++ SpringMvc03/src/main/java/com/xiaoren/SpringMvc03Application.java
@@ -0,0 +1,13 @@
+package com.xiaoren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc03Application.class, args);
+    }
+
+}
